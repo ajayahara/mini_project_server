97c9a6caaa67befdcfdf78366bca388bbd4139bb
ajayahara <abak00350@gmail.com>
2025-07-12 08:53:55 +0530

    cookie config for production

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -11,7 +11,8 @@ exports.register = async (req, res) => {
   const { email, password } = req.body;
   try {
     const userExists = await UserModel.findOne({ email });
-    if (userExists) return res.status(400).json({ message: "Email already in use" });
+    if (userExists)
+      return res.status(400).json({ message: "Email already in use" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await UserModel.create({ email, password: hashedPassword });
@@ -19,14 +20,18 @@ exports.register = async (req, res) => {
     const token = createToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false, 
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
-    res.status(201).json({ message: "User registered", user: { email: user.email } });
+    res
+      .status(201)
+      .json({ message: "User registered", user: { email: user.email } });
   } catch (err) {
-    res.status(500).json({ message: "Registration failed", error: err.message });
+    res
+      .status(500)
+      .json({ message: "Registration failed", error: err.message });
   }
 };
 
@@ -42,8 +47,8 @@ exports.login = async (req, res) => {
     const token = createToken(user._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
